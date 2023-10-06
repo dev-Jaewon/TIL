c0491eae4a3958c0a2397502fd827a9c2c3e8e4c
dev-Jaewon <app235naver@gmail.com>
2023-10-06 16:50:00 +0900

    feat springboot study 2023-10-6

---
+++ SpringBoot/study_03.java
@@ -0,0 +1,19 @@
+package SpringBoot;
+
+public class study_03 {
+    public static void main(String[] args) {
+        // REST API contoller 생성 방법
+        // @RestController
+        // public class FirstApiController {
+        //     @GetMapping("/api/hello")
+        //     public String hello(){
+        //         return "hello";
+        //     }
+        // }
+
+
+        // Controller와 RestController의 차이
+        // Controller, body에 html 코드가 있다.
+        // RestController, return 문자열이 있다.
+    }
+}
